d527201d3688283fc813ea8a67a47de045ff84af
Rumman Waqar <rumman.waqar05@gmail.com>
2018-03-31 12:57:55 -0600

    removed i2c_master.c

--- firmware/self-balancing-robot/self-balancing-robot/main.c
+++ firmware/self-balancing-robot/self-balancing-robot/main.c
@@ -8,6 +8,7 @@
 #include "defines.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
@@ -18,6 +19,10 @@
 #include "i2cmaster.h"
 
 FILE uart_stream = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_RW);
+
+#define ADDR (0x68 << 1)
+#define MPU6050_RA_WHO_AM_I         0x75
+
 
 void gpio_init(void);
 
@@ -30,39 +35,30 @@ int main(void)
 	
 	sei();
 	
-	uint8_t address = 0;
-	
-	// initialize encoder
-	DDRD &= ~( _BV(PORTD2) );
+	char buffer[10];
+	unsigned char ret;
 
     while (1) 
     {
-		if(PIND & _BV(2))
+		if(i2c_start(ADDR | I2C_WRITE) == 0)
 		{
-			PORT(LED_PORT) |= _BV(LED_GREEN);
+			//i2c_write(MPU6050_RA_WHO_AM_I);
+			//_delay_us(10);
+			//i2c_rep_start(ADDR | I2C_READ);
+			//ret = i2c_readNak();                    // read one byte from EEPROM
+			i2c_stop();
+			//itoa(ret, buffer, 16);
+			//printf("Result: 0x%s\n", buffer);
 		}
 		else
 		{
-			PORT(LED_PORT) &= ~(_BV(LED_GREEN));
+			printf("Failed");
 		}
-		//i2c_rep_start((address << 1)+I2C_READ);       // set device address and read mode
-		//unsigned char ret = i2c_readNak();                    // read one byte from EEPROM
-		//i2c_stop();
-		//if(ret != 0)
-		//{
-			//puts("Found something");
-		//}
-		//
-		//
-		//address++;
-		//if(address > 127)
-		//{
-			//address = 0;
-			//puts("nothing found");
-		//}	
 		
-		//PORT(LED_PORT) ^= _BV(LED_RED);
-		//_delay_ms(200);
+		
+		
+		PORT(LED_PORT) ^= _BV(LED_RED);
+		_delay_ms(100);
     }
 }
 
